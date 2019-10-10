3872595d9342a47c48b10ce5edd7f58cf9998b48
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-10-09 19:03:34 -0500

    tidy up the file deletion helper function

--- src/util/util.ts
+++ src/util/util.ts
@@ -20,13 +20,15 @@ export async function filterImageFromURL(url: string): Promise<string> {
     });
 }
 
-// deleteLocalFiles
-// helper function to delete files on the local disk
-// useful to cleanup after tasks
-// INPUTS
-//    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
+/**
+ * Helper function to delete files on the local disk. Useful to cleanup after
+ * tasks.
+ *
+ * @param {string[]} files An array of absolute paths to files
+ * @returns {void}
+ */
+export async function deleteLocalFiles(files: Array<string>): Promise<void> {
+    for (let file of files) {
         fs.unlinkSync(file);
     }
 }
